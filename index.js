cc912ca5c06b9ebf511a48279d639aa0625e900e
maritrombini <marianagsena@gmail.com>
2021-11-16 21:40:04 -0300

    first routes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express() //cria uma aplicacao express
 const port = 8080
 
-//app ouça a porta =]
-app.listen(port, function () {
-  console.log('Ouvindo a porta: ', port)
-})
+// GET - Obter -  costuma usar query string
+// POST - Criar
+// PUT - Atualizar - costuma usar params para passar identificador
+// DELETE  - Deletar
+
+app.use(express.json())
 
 app.get('/', function (req, res) {
   res.send('Minha primeira requisição')
@@ -16,6 +18,31 @@ app.get('/segunda-req', function (req, res) {
 })
 
 app.get('/com-parametros', function (req, res) {
+  if (req.query.nome === 'Mariana') {
+    res.send('Mariana chamou a requisição')
+  }
   //req.query.nome
-  res.send('Com parâmetros funciona! Sabadou ' + req.query.nome)
+  res.send(
+    'Com parâmetros funciona! Sabadou ' + req.query.nome + req.query.sobrenome
+  )
+})
+
+app.post('/meu-primeiro-post', function (req, res) {
+  console.log(req.body)
+  res.send('Meu post funciona')
+})
+
+app.put('/meu-primeiro-put/:id', function (req, res) {
+  console.log(req.body, req.params.id)
+  res.send('Meu put funciona')
+})
+
+app.delete('/meu-primeiro-delete/:id', function (req, res) {
+  console.log(req.params.id)
+  res.send('Meu delete funciona ' + req.params.id)
+})
+
+//app ouça a porta =]
+app.listen(port, function () {
+  console.log('Ouvindo a porta: ', port)
 })
